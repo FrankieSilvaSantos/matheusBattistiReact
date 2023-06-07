0986521c9990aa1cf2d5e35d73e543511cfd33a9
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-06-07 03:10:41 -0300

    useState

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,39 @@
+import { useState } from "react"
+
 function Form() {
 
 
     function cadastrarUsuario(e) {
         e.preventDefault()
-        let input = document.getElementById('inputText').value
-        console.log(`Parabains se cadastrou!! ${input}`)
+        let input = document.getElementById('name').value
+        let password = document.getElementById('password').value
+        console.log(`Usuario: ${input} Senha: ${password}`)
 
     }
 
-    return(
-        <>
-        <h1>Meu cadastro</h1>
-        <form onSubmit={cadastrarUsuario}>
-            <div>
-        <input type="text" id="inputText" placeholder="digite o seu nome" pattern="[a-zA-Z]+[ ][a-zA-Z]+" minLength="7" maxLength="25" required >
-        
-        </input>
-        </div>
+    const [name,setName] = useState()
+    const [password,setPassword] = useState()
 
-        <input type="submit" value="cadastrar" >
-        
-        </input>
-        </form>
+    return (
+        <>
+            <h1>Meu cadastro</h1>
+            <form onSubmit={cadastrarUsuario}>
+                <div>
+                    <label htmlFor="name">Nome: </label>
+                    <input type="text" id="name" placeholder="digite o seu nome" pattern="[a-zA-Z]+[ ][a-zA-Z]+"
+                     minLength="7" maxLength="25" required onChange={(e) => setName(e.target.value)}></input>
+                </div>
+
+                <div>
+                    <label htmlFor="password">Senha: </label>
+                    <input type="password" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+                    placeholder="digite a sua senha" required onChange={((e) => setPassword(e.target.value))}></input>
+                </div>
+
+                <input type="submit" value="cadastrar" >
+
+                </input>
+            </form>
         </>
     )
 }
